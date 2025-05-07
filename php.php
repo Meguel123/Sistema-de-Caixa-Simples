7889f66fe2d641a631fc07df6e35674239c58733
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-07 14:54:04 -0300

    b

--- php.php
+++ php.php
@@ -1,6 +1,19 @@
-<body>
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+    <style>
 
+</style>
+</head>
+<body style="background: #f4f4f9;">
+
+    
+<div style="align-items: center; margin-left: 40%; margin-top: 13%; border: 5px solid; border-color: #5a5a5a; margin-right: 40%; padding: 1%;  border-radius: 10px; background-color: rgb(218, 218, 218);">
+    <div style="margin-left: 10%; align-items: center;">
+    <h1>Dados Coletados</h1>
+<?php
 $nome = $_POST["nome_cliente"];
 $PRODUTO1 = $_POST["nome_produto"];
 $VPRODUTO1 = $_POST["Vproduto1"];
@@ -9,8 +22,6 @@ $VPRODUTO2 = $_POST["Vproduto2"];
 $V = $_POST["V"];
 $total = $VPRODUTO1 + $VPRODUTO2;
 $troco = $V - $total;
-
-
 
 echo "seu nome é :". $nome;
 echo "<br>";
@@ -36,3 +47,5 @@ echo "<br>";
 echo "o troco é :". $troco;
 
 ?>
+</div>
+</div>
